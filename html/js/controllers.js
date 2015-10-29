403c9fb9d1924131ad00fc06deffc25d4250fc55
Dan <dstein64@users.noreply.github.com>
2015-10-29 13:49:39 -0400

    decouple json

--- html/js/controllers.js
+++ html/js/controllers.js
@@ -1,46 +1,20 @@
-app.controller('MainController', ['$scope', function($scope) {
-  $scope.title = 'My Top Sellers in Books';
-  $scope.promo = "MyOwnString";
-  $scope.products = [ 
-  { 
-    name: 'The Book of Trees', 
-    price: 19,
-    pubdate: new Date('2014', '03', '08'), 
-    cover: 'http://www.bookoftrees.info/bt/images/cover_640.jpg',
-    likes: 0,
-    dislikes: 0
-  }, 
-  { 
-    name: 'Program or be Programmed', 
-    price: 8,
-    pubdate: new Date('2013', '08', '01'), 
-    cover: 'http://www.rushkoff.com/storage/program_sm.jpg',
-    likes: 0,
-    dislikes: 0
-  },
-    {
-      name: 'The Alchemist',
-      price: 12,
-      pubdate: new Date('1988', '01', '01'),
-      cover: 'https://upload.wikimedia.org/wikipedia/en/c/c4/TheAlchemist.jpg',
-      likes: 0,
-      dislikes: 0
-    },
-    {
-      name: 'Harry Potter',
-      price: 10,
-      pubdate: new Date('1997', '01', '01'),
-      cover: 'https://upload.wikimedia.org/wikipedia/en/6/6b/Harry_Potter_and_the_Philosopher%27s_Stone_Book_Cover.jpg',
-      likes: 0,
-      dislikes: 0
+app.controller('MainController', function($scope, $http) {
+    $http.get('products.json').success(function(data) {
+        $scope.products = data;
+    });
+
+
+    $scope.title = 'My Top Sellers in Books';
+    $scope.promo = "MyOwnString";
+
+
+
+    $scope.plusOne = function(index) {
+        $scope.products[index].likes += 1;
     }
-];
-  $scope.plusOne = function(index) {
-    $scope.products[index].likes += 1;
-  }
-  
-  $scope.minusOne = function(index) {
-    $scope.products[index].dislikes += 1;
-  }
-  
-}]);
+
+    $scope.minusOne = function(index) {
+        $scope.products[index].dislikes += 1;
+    }
+});
+
